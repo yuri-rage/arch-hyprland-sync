ba846300a3a7a1bf05bd5d099b8976d9cd5dc81f
Yuri Rage <yuri.rage@gmail.com>
2025-06-15 11:02:28 -0500

    migrate to blink.cmp

--- config/nvim/dot-config/nvim/lua/plugins/lsp-config.lua
+++ config/nvim/dot-config/nvim/lua/plugins/lsp-config.lua
@@ -1,53 +1,57 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        lazy = false,
-        config = function()
-            require("mason").setup()
-        end,
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        lazy = false,
-        opts = {
-            auto_install = true,
-        },
-    },
-    {
-        "neovim/nvim-lspconfig",
-        lazy = false,
-        config = function()
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
-            local lspconfig = require("lspconfig")
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		opts = {
+			auto_install = true,
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		dependencies = {
+			"saghen/blink.cmp",
+		},
+		config = function()
+			-- local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            local capabilities = require("blink.cmp").get_lsp_capabilities()
+			local lspconfig = require("lspconfig")
 
-            -- autoformat on save for lsp supported files
-            ---@diagnostic disable-next-line: unused-local
-            local on_attach = function(client, bufnr)
-                if client.supports_method("textDocument/formatting") then
-                    vim.cmd([[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]])
-                end
-            end
+			-- autoformat on save for lsp supported files
+			---@diagnostic disable-next-line: unused-local
+			local on_attach = function(client, bufnr)
+				if client.supports_method("textDocument/formatting") then
+					vim.cmd([[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]])
+				end
+			end
 
-            lspconfig.ts_ls.setup({
-                capabilities = capabilities,
-                on_attach = on_attach,
-            })
-            lspconfig.html.setup({
-                capabilities = capabilities,
-                on_attach = on_attach,
-            })
-            lspconfig.lua_ls.setup({
-                capabilities = capabilities,
-                on_attach = on_attach,
-            })
-            lspconfig.clangd.setup({
-                capabilities = capabilities,
-                on_attach = on_attach,
-            })
-            lspconfig.pylsp.setup({
-                capabilities = capabilities,
-                on_attach = on_attach,
-            })
-        end,
-    },
+			lspconfig.ts_ls.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+			})
+			lspconfig.html.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+			})
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+			})
+			lspconfig.clangd.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+			})
+			lspconfig.pylsp.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+			})
+		end,
+	},
 }
